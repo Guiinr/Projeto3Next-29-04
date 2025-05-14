50f92b41c4e2342203adbfffd987b45627a9e16c
Guiinr <ribeiro.n@aluno.ifsp.edu.br>
2025-05-14 16:49:14 -0300

    ajustes home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,27 +9,23 @@ export default function Home() {
 
     return (
         <div className={styles.container}>
-            <span className={styles.letterS}>A</span>
+            <span className={styles.letterS}>T</span>
             <img
-                src="/ivan.png"
+                src="/logo.png"
                 alt="header"
                 className={styles.headerImage}
                 width={400}
                 height={400} // Adicione dimensões para otimização
             />
 
-            <h4 className={styles.textLeft}>IVAN</h4>
-            <h4 className={styles.textRight}>RTS</h4>
+            <h4 className={styles.textLeft}>AR</h4>
+            <h4 className={styles.textRight}>FY</h4>
 
             <Link href="/shop" className={`${styles.btn} ${styles.explore}`}>
                 EXPLORE MORE
             </Link>
 
-            <h5 className={styles.feature1}>Diesel</h5>
-            <h5 className={styles.feature2}>Watches</h5>
-            <h5 className={styles.feature3}>Tough</h5>
-            <h5 className={styles.feature4}>Modern</h5>
-
+            
             <footer className={styles.footer}>
                 <div className={styles.footerLinks}>
                     <li><a href="#">Facebook</a></li>
